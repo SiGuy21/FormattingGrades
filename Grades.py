7356a73bf89eb98f0db5e5e641d45ac3c003143c
JM3737 <joshua1110041331@gmail.com>
2025-03-31 14:26:34 -0600

    Update Grades.py
    This is a code for creating new sheets and saving the excel file.

--- Grades.py
+++ Grades.py
@@ -18,10 +18,17 @@ myWorkbook.remove(myWorkbook["Sheet"])
 
 
 # 1. Sheets for each class
-algebraSheet = myWorkbook.create_sheet("Algebra")
-trigonometrySheet = myWorkbook.create_sheet("Trigonometry")
-geometrySheet = myWorkbook.create_sheet("Geometry")
-calculusSheet = myWorkbook.create_sheet("Calculus")
+sheetNames = ["Algebra", "Calculus", "Trigonometry", "Geometry"]
+
+# Loop through to add sheet if it doesn't exist already
+for sheet in sheetNames:
+    if sheet not in myWorkbook.sheetnames: # I got help from AI for this part (Joshua Min)
+        myWorkbook.create_sheet(title=sheet)
+        
+# Remove default sheet
+dSheet = myWorkbook.active
+if dSheet.title == "Sheet":
+    myWorkbook.remove(dSheet)
 
 # 2. last name, first name, student ID, and grade columns
 pass
